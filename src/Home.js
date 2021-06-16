dd05c36e7a5d9ad0d289e0ec56704ff2647a8635
shlok01 <shlok0196.com>
2021-06-16 19:47:38 +0530

    restructure homepage, add styles

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My New Website", body: "lorem ipsum...", author: "mario", id: 1 },
+    {
+      title: "Welcome React Dev Tools",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Something On github",
+      body: "lorem ipsum...",
+      author: "punto",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2 style={{
-        textAlign: 'center'
-      }}>Homepage</h2>
+      <h2 style={{textAlign: 'center'}}> Homepage</h2>
+      <BlogList blogs={blogs} />
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,16 @@ const Navbar = () => {
       <h1>The Doge Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create" style={{
-            color: 'white',
-            backgroundColor: 'teal',
-            borderRadius: '8px'
-        }}>New Blog</a>
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "teal",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
